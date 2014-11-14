afbb2fb0223567cbf3599452f3619d8ab26c1d80
Daniel Budden <thedb317@hotmail.com>
2014-11-14 14:51:04 +1100

    Updating artist route, test promise based async

--- routes/artist.js
+++ routes/artist.js
@@ -1,10 +1,16 @@
 var Artist =  require('../data/models/artist');
+var Q = require('q');  // Promise library
+
+// wrap callback style functions to return a promise
+var findArtist = Q.denodeify(Artist.find);
+var findOneArtist = Q.denodify(Artist.findOne);
 
 module.exports = function(app) {
 
     app.get('/artists', function(req, res) {
-        var artist_list = [];
-        Artist.find({}, function(err, artists) {
+        var promise = findArtist({sort: {name: 1}});
+        promise.then(processArtists, processError);
+        /*Artist.find({}, function(err, artists) {
             if(err) {
                 console.log(err);
             }
@@ -18,11 +24,13 @@ module.exports = function(app) {
                 artist_list.push(record);
             });
             res.end(JSON.stringify(artist_list));
-        });
+        }); */
     });
 
     app.get('/artists/:artist', function(req, res) {
-        Artist.findOne({tag: req.params.artist}, function(err, artist) {
+        var promise = findOneArtist({tag: req.params.artist});
+        promise.then(processAlbums, processError);
+        /*Artist.findOne({tag: req.params.artist}, function(err, artist) {
             if(err) {
                 console.log(err);
             }
@@ -37,11 +45,13 @@ module.exports = function(app) {
                 });
                 res.end(JSON.stringify(record));
             }
-        });
+        });*/
     });
 
     app.get('/artists/:artist/:album', function(req, res) {
-        Artist.findOne({tag: req.params.artist}, function(err, artist) {
+        var promise = findOneArtist({tag: req.param.artist});
+        promise.then(processSongs, processError);
+        /*Artist.findOne({tag: req.params.artist}, function(err, artist) {
             if(err) {
                 console.log(err);
             }
@@ -61,6 +71,53 @@ module.exports = function(app) {
                     }
                 }
             }
-        });
+        });*/
     });
+};
+
+// promise fulfillment/rejection functions not exported
+function processArtists(artists) {
+    var artist_list = [];
+    artists.forEach(function(artist) {
+        var record = {name: artist.name, tag: artist.tag, _id: artist._id};
+        artist_list.push(record);
+    });
+    res.end(JSON.stringify(artist_list));
+}
+
+function processAlbums(artist) {
+    if(!artist) {
+        res.end("404");
+    }
+    else {
+        var record = {name: artist.name, tag: artist.tag, genre: artist.genre, albums: []};
+        artist.albums.forEach(function(album) {
+            var album_record = {a_name: album.a_name, tag: album.tag};
+            record.albums.push(album_record);
+        });
+        res.end(JSON.stringify(record));
+    }
+}
+
+function processSongs(artist) {
+    if(!artist) {
+        res.end("404");
+    }
+    else {
+        for(var i=0; i < artist.albums.length; i++) {
+            var album = artist.albums[i];
+            if(album.tag == req.params.album) {
+                var record = {name: artist.name, tag: artist.tag, a_name: album.a_name, a_tag: album.tag, songs: []};
+                album.songs.forEach(function(song) {
+                    record.songs.push({number: song.s_number, name: song.s_name, filename: song.filename});
+                });
+                return res.end(JSON.stringify(record));
+            }
+        }
+        res.end("404"); // album not found
+    }
+}
+
+function processError(err) {
+    console.log(err);
 }
